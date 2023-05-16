7eb81aecf6b867991a613224fb0e9006b08d6430
FreddyxD5 <freddyxd5@gmail.com>
2023-05-16 10:59:33 -0500

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [text, onChangeText] = React.useState('Ingrese su celular');
+  const [number, onChangeNumber] = React.useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textColor}>Holitas de mar from Peru siuuuu</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="siuuuu"
+        keyboardType="numeric"
+      />
     </View>
   );
 }
@@ -13,8 +24,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#20232a',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textColor:{
+    color: '#f05035',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
