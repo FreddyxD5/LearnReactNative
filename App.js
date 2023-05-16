7cc3b5c9052979c90dfc6c61218b5444b4653b94
FreddyxD5 <freddyxd5@gmail.com>
2023-05-16 12:47:23 -0500

    Flexbox intro

--- App.js
+++ App.js
@@ -1,40 +1,73 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   const [text, onChangeText] = React.useState('Ingrese su celular');
   const [number, onChangeNumber] = React.useState('');
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.textColor}>Holitas de mar from Peru siuuuu</Text>
-      <StatusBar style="auto" />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="siuuuu"
-        keyboardType="numeric"
-      />
+    <View style={{
+        padding: 50,
+        flexDirection:'row',
+        width:'80%',
+        height:300,
+        justifyContent:'space-around',
+        alignItems:'stretch'
+      }}>      
+      <View style={{
+        backgroundColor:'red',        
+        justifyContent: 'center',        
+        flex:1,
+        flexDirection:'row',
+        
+        alignItems: 'center'
+      }}>
+        <Text>1</Text>
+        <Text>other</Text>
+      </View>
+      <View style={{
+        backgroundColor:'blue',         
+        justifyContent: 'center',
+        flex:1,
+        alignItems: 'center'
+      }}>
+        <Text>2</Text>
+      </View>
+      <View style={{
+        backgroundColor:'green',        
+        justifyContent: 'center',
+        flex:1,
+        alignItems: 'center'
+      }}>
+        <Text>3</Text>
+      </View>          
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#20232a',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles = StyleSheet.create({  
+  appContainer: {
+    padding:50,
   },
-  textColor:{
-    color: '#f05035',
+  inputContainer: {
+    flexDirection:'row',
+    justifyContent:'space-between'
   },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
+  textInput:{
+    borderWidth:1,
+    borderColor: '#cccccc',
+    width:'80%',
+    marginRight: 8,
+    padding: 8,
+  }
 });
+
+{/* <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!!!" />
+        <Button title="Add Goal"/>
+      </View>
+      <View></View>
+      <View>
+        <Text>List of Goals!</Text>
+      </View> */}
